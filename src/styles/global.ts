e3e5a8702720e02278d35f174c4f75ee7a9cded1
thewanionly <pelloani@gmail.com>
2023-05-22 03:13:13 +0800

    feat: add typography related global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -13,13 +13,73 @@ export const GlobalStyles = createGlobalStyle`
 
   html {
     box-sizing: border-box;
+
+    /**
+     * Set root font-size to 10px.
+     * Default browser font size is 16px and 62.5% of 16px is 10px
+     * This makes 1rem = 10px.
+     * We use percentage here so our root font size adapts if ever browser font setting is changed.
+     * Source: https://www.aleksandrhovhannisyan.com/blog/62-5-percent-font-size-trick/
+     */
+    font-size: 62.5%;
+
+    & * {
+      font-size: ${({ theme: { fontSizes } }) => fontSizes.regular};
+      font-weight: ${({ theme: { fontWeights } }) => fontWeights.regular};
+    }
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    text-transform: uppercase;
+    font-weight: ${({ theme: { fontWeights } }) => fontWeights.bold};
+  }
+
+  h1 {
+    font-size: ${({ theme: { fontSizes } }) => fontSizes.xxxl};
+    line-height: 5.8rem;
+    letter-spacing: 0.2rem;
+  }
+
+  h2 {
+    font-size: ${({ theme: { fontSizes } }) => fontSizes.xxl};
+    line-height: 4.4rem;
+    letter-spacing: 0.15rem;
+  }
+
+  h3 {
+    font-size: ${({ theme: { fontSizes } }) => fontSizes.xl};
+    line-height: 3.6rem;
+    letter-spacing: 0.115rem;
+  }
+
+  h4 {
+    font-size: ${({ theme: { fontSizes } }) => fontSizes.lg};
+    line-height: 3.8rem;
+    letter-spacing: 0.2rem;
+  }
+
+
+  h5 {
+    font-size: ${({ theme: { fontSizes } }) => fontSizes.med2};
+    line-height: 3.3rem;
+    letter-spacing: 0.17rem;
+  }
+
+  h6 {
+    font-size: ${({ theme: { fontSizes } }) => fontSizes.med1};
+    line-height: 2.5rem;
+    letter-spacing: 0.13rem;
   }
 
   button {
     cursor: pointer;
     border: none;
   }
-
 
   ul {
     list-style: none;
