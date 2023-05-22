6e887373e2de594995e20d6b47cf940c7123fde3
thewanionly <pelloani@gmail.com>
2023-05-23 04:18:04 +0800

    feat: update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from 'next'
 import { Manrope } from 'next/font/google'
 
 import StyledComponentsRegistry from '@/lib/styledComponentsRegistry'
@@ -5,9 +6,29 @@ import { GlobalStyles, Theme } from '@/styles'
 
 const manrope = Manrope({ subsets: ['latin'], display: 'swap' })
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Audiophile',
   description: 'An e-commerce web application',
+  icons: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      url: '/favicon/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      url: '/favicon/favicon-16x16.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      url: '/favicon/apple-touch-icon.png',
+    },
+  ],
+  manifest: '/favicon/site.webmanifest',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
