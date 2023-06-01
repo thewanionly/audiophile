1977733cd6e6804010427d0ee498de7aca933b23
thewanionly <pelloani@gmail.com>
2023-06-02 04:41:00 +0800

    feat: update StyleRegistry to not use mantine

--- src/lib/styleRegistry/StyleRegistry.tsx
+++ src/lib/styleRegistry/StyleRegistry.tsx
@@ -1,33 +1,29 @@
 'use client'
 
-// import { CacheProvider } from '@emotion/react'
-// import { useServerInsertedHTML } from 'next/navigation'
+import { useServerInsertedHTML } from 'next/navigation'
+import createCache from '@emotion/cache'
+import { CacheProvider, ThemeProvider } from '@emotion/react'
 
 import { theme } from '@/styles'
-import { ThemeProvider } from '@emotion/react'
 
-// export const StyleRegistry = ({ children }: { children: React.ReactNode }) => {
-//   const cache = useEmotionCache()
-//   cache.compat = true
-
-//   useServerInsertedHTML(() => (
-//     <style
-//       data-emotion={`${cache.key} ${Object.keys(cache.inserted).join(' ')}`}
-//       dangerouslySetInnerHTML={{
-//         __html: Object.values(cache.inserted).join(' '),
-//       }}
-//     />
-//   ))
-
-//   return (
-//     <CacheProvider value={cache}>
-//       <MantineProvider withGlobalStyles withNormalizeCSS theme={{ other: theme }}>
-//         {children}
-//       </MantineProvider>
-//     </CacheProvider>
-//   )
-// }
+const createEmotionCache = () => createCache({ key: 'css', prepend: true })
 
 export const StyleRegistry = ({ children }: { children: React.ReactNode }) => {
-  return <ThemeProvider theme={{ other: theme }}>{children}</ThemeProvider>
+  const cache = createEmotionCache()
+  cache.compat = true
+
+  useServerInsertedHTML(() => (
+    <style
+      data-emotion={`${cache.key} ${Object.keys(cache.inserted).join(' ')}`}
+      dangerouslySetInnerHTML={{
+        __html: Object.values(cache.inserted).join(' '),
+      }}
+    />
+  ))
+
+  return (
+    <CacheProvider value={cache}>
+      <ThemeProvider theme={{ other: theme }}>{children}</ThemeProvider>
+    </CacheProvider>
+  )
 }
