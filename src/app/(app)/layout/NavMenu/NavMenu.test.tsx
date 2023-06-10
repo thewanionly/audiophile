91b790da2bab733b84b5594cba97618db05563e0
thewanionly <pelloani@gmail.com>
2023-06-11 00:07:19 +0800

    test: add NavMenu tests

--- src/app/(app)/layout/NavMenu/NavMenu.test.tsx
+++ src/app/(app)/layout/NavMenu/NavMenu.test.tsx
@@ -1,8 +1,22 @@
 import { render, screen } from '@/tests'
 import { NavMenu } from './NavMenu'
 
+// Mock "useLayoutContext"
+jest.mock('../Layout.context', () => ({
+  __esModule: true,
+  useLayoutContext: jest.fn(() => ({
+    isNavMenuOpen: true,
+  })),
+}))
+
 describe('NavMenu', () => {
-  it('displays the nav items', () => {
+  it('displays category card list', () => {
     render(<NavMenu />)
+
+    const categoryCardList = screen.getByTestId('category-card-list')
+    const categoryCards = screen.getAllByTestId('category-card')
+
+    expect(categoryCardList).toBeInTheDocument()
+    expect(categoryCards.length).toBeGreaterThan(0)
   })
 })
