93fc686f8781ad539198ca249c8c3833db6fedc7
thewanionly <pelloani@gmail.com>
2023-06-15 03:59:26 +0800

    feat: add social icons

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -2,15 +2,24 @@ import styled from '@emotion/styled'
 
 import ArrowRight from 'public/icons/arrow-right.svg'
 import CartIcon from 'public/icons/cart.svg'
+import FacebookIcon from 'public/icons/facebook.svg'
+import InstagramIcon from 'public/icons/instagram.svg'
+import TwitterIcon from 'public/icons/twitter.svg'
 
 export enum IconName {
   ArrowRight = 'arrow_right',
   Cart = 'cart',
+  Facebook = 'facebook',
+  Instagram = 'instagram',
+  Twitter = 'twitter',
 }
 
 const IconMap = {
   [IconName.ArrowRight]: ArrowRight,
   [IconName.Cart]: CartIcon,
+  [IconName.Facebook]: FacebookIcon,
+  [IconName.Instagram]: InstagramIcon,
+  [IconName.Twitter]: TwitterIcon,
 } as const
 
 type IconProps = {
