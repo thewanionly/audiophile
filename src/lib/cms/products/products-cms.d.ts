cbc77d598c9f3c46ceca6002233f721997795f69
thewanionly <pelloani@gmail.com>
2023-06-17 03:58:57 +0800

    feat: update product schema

--- src/lib/cms/products/products-cms.d.ts
+++ src/lib/cms/products/products-cms.d.ts
@@ -1,6 +1,33 @@
+interface ResponsiveImage {
+  mobile: string
+  tablet: string
+  desktop: string
+}
+
+interface Inclusion {
+  quantity: number
+  item: string
+}
+
+interface Gallery {
+  first: ResponsiveImage
+  second: ResponsiveImage
+  third: ResponsiveImage
+}
+
 interface Product {
   id: number
   slug: string
   name: string
+  image: ResponsiveImage
   category: string
+  categoryImage: ResponsiveImage
+  new: boolean
+  price: number
+  description: string
+  features: string
+  includes: Inclusion[]
+  gallery: Gallery[]
+  others: string[]
+  thumbnailImage: ResponsiveImage
 }
--- src/lib/cms/products/schema/product.ts
+++ src/lib/cms/products/schema/product.ts
@@ -1,3 +1,21 @@
+const responsiveImageFields = [
+  {
+    title: 'Mobile',
+    name: 'mobile',
+    type: 'string',
+  },
+  {
+    title: 'Tablet',
+    name: 'tablet',
+    type: 'string',
+  },
+  {
+    title: 'Desktop',
+    name: 'desktop',
+    type: 'string',
+  },
+]
+
 export const product = {
   title: 'Product',
   name: 'product',
@@ -19,9 +37,101 @@ export const product = {
       type: 'string',
     },
     {
+      title: 'Image',
+      name: 'image',
+      type: 'object',
+      fields: responsiveImageFields,
+    },
+    {
       title: 'Category',
       name: 'category',
       type: 'string',
+    },
+    {
+      title: 'Category Image',
+      name: 'categoryImage',
+      type: 'object',
+      fields: responsiveImageFields,
+    },
+    {
+      title: 'New',
+      name: 'new',
+      type: 'boolean',
+    },
+    {
+      title: 'Price',
+      name: 'price',
+      type: 'number',
+    },
+    {
+      title: 'Description',
+      name: 'description',
+      type: 'text',
+    },
+    {
+      title: 'Features',
+      name: 'features',
+      type: 'text',
+    },
+    {
+      title: 'Includes',
+      name: 'includes',
+      type: 'array',
+      of: [
+        {
+          type: 'object',
+          name: 'include',
+          fields: [
+            {
+              title: 'Quantity',
+              name: 'quantity',
+              type: 'number',
+            },
+            {
+              title: 'Item',
+              name: 'item',
+              type: 'string',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      title: 'Gallery',
+      name: 'gallery',
+      type: 'object',
+      fields: [
+        {
+          title: 'First',
+          name: 'first',
+          type: 'object',
+          fields: responsiveImageFields,
+        },
+        {
+          title: 'Second',
+          name: 'second',
+          type: 'object',
+          fields: responsiveImageFields,
+        },
+        {
+          title: 'Third',
+          name: 'third',
+          type: 'object',
+          fields: responsiveImageFields,
+        },
+      ],
+    },
+    {
+      title: 'Others',
+      name: 'others',
+      type: 'array',
+      of: [{ type: 'string' }],
+    },
+    {
+      title: 'Thumbnail Image',
+      name: 'thumbnailImage',
+      type: 'object',
+      fields: responsiveImageFields,
     },
   ],
 }
