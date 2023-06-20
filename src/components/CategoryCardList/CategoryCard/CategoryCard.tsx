04d2264942679a305d55e8ba74d09ce05a194431
thewanionly <pelloani@gmail.com>
2023-06-21 03:22:38 +0800

    refactor: update CategoryCard styling

--- src/components/CategoryCardList/CategoryCard/CategoryCard.tsx
+++ src/components/CategoryCardList/CategoryCard/CategoryCard.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 
 import { Icon, IconName } from '@/components'
 import { ResponsiveImage } from '@/components/ResponsiveImage'
+import { mediaQuery } from '@/styles/utils'
 
 const S = {
   CategoryCard: styled.div`
@@ -12,11 +13,17 @@ const S = {
     justify-items: center;
   `,
   CategoryCardImageContainer: styled.div`
-    top: 1.2rem;
-    width: 12rem;
-    height: 12rem;
+    top: 2rem;
+    width: 14rem;
+    height: 14rem;
     position: relative;
     grid-area: 1 / 1 / 3 / 2;
+
+    ${({ theme }) => mediaQuery(theme.breakPoints.desktop)} {
+      top: 2rem;
+      width: 16rem;
+      height: 16rem;
+    }
   `,
   CategoryCardImage: styled(ResponsiveImage)`
     object-fit: contain;
@@ -27,16 +34,25 @@ const S = {
 
     grid-area: 2 / 1 / 4 / 2;
     width: 100%;
+    height: 16.5rem;
+
+    ${({ theme }) => mediaQuery(theme.breakPoints.desktop)} {
+      height: 20.4rem;
+    }
   `,
   CategoryCardTextContainer: styled.div`
     grid-area: 3 / 1 / 4 / 2;
     width: 100%;
 
     padding: 2.2rem;
-    padding-top: 1rem;
+    padding-top: 0;
     display: flex;
     flex-direction: column;
     align-items: center;
+
+    ${({ theme }) => mediaQuery(theme.breakPoints.desktop)} {
+      padding-bottom: 3rem;
+    }
   `,
   CategoryCardName: styled.span`
     font-weight: ${({ theme }) => theme.fontWeights.bold};
@@ -47,6 +63,12 @@ const S = {
     color: ${({ theme }) => theme.colors.darkTitle};
 
     margin-bottom: 1.7rem;
+
+    ${({ theme }) => mediaQuery(theme.breakPoints.desktop)} {
+      font-size: ${({ theme }) => theme.fontSizes.med1};
+      line-height: 2.5rem;
+      letter-spacing: 0.1286rem;
+    }
   `,
 
   CategoryCardShopLink: styled(Link)`
