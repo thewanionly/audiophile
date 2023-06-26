d9fb7b079a9959264b2d0295834b7358baa8b1f0
thewanionly <pelloani@gmail.com>
2023-06-27 05:17:32 +0800

    feat: add mock data for all featured products

--- src/tests/__mocks__/data/home.ts
+++ src/tests/__mocks__/data/home.ts
@@ -17,3 +17,59 @@ export const mockedHeroSectionData: ProductSectionData = {
     alt: 'headphones alt',
   },
 }
+
+export const mockedPrimaryFPSectionData: ProductSectionData = {
+  product: {
+    id: 6,
+    slug: 'zx9-speaker',
+    name: 'ZX9 Speaker M',
+    category: 'speakers',
+    new: true,
+  },
+  message:
+    'Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound mocked.',
+  sectionImage: {
+    src: {
+      mobile: '/speakers-image.jpg',
+      tablet: '/speakers-image.jpg',
+      desktop: '/speakers-image.jpg',
+    },
+    alt: 'speakers alt',
+  },
+}
+
+export const mockedSecondaryFPSectionData: Omit<ProductSectionData, 'message'> = {
+  product: {
+    id: 5,
+    slug: 'zx7-speaker',
+    name: 'ZX7 Speaker',
+    category: 'speakers',
+    new: false,
+  },
+  sectionImage: {
+    src: {
+      mobile: '/speakers-secondary-image.jpg',
+      tablet: '/speakers-secondary-image.jpg',
+      desktop: '/speakers-secondary-image.jpg',
+    },
+    alt: 'speakers secondary alt',
+  },
+}
+
+export const mockedTertiaryFPSectionData: Omit<ProductSectionData, 'message'> = {
+  product: {
+    id: 1,
+    slug: 'yx1-earphones',
+    name: 'YX1 Wireless Earphones',
+    category: 'earphones',
+    new: true,
+  },
+  sectionImage: {
+    src: {
+      mobile: '/earphones-image.jpg',
+      tablet: '/earphones-image.jpg',
+      desktop: '/earphones-image.jpg',
+    },
+    alt: 'earphones alt',
+  },
+}
