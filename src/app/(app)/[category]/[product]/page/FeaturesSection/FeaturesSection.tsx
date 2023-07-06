4ecfd5cfa51a7921cb616d1c0bc8aa793031af4c
thewanionly <pelloani@gmail.com>
2023-07-07 04:22:37 +0800

    feat: update styling of features section

--- src/app/(app)/[category]/[product]/page/FeaturesSection/FeaturesSection.tsx
+++ src/app/(app)/[category]/[product]/page/FeaturesSection/FeaturesSection.tsx
@@ -19,8 +19,32 @@ const S = {
       margin: 16rem auto;
     }
   `,
-  FeaturesHeading: styled.h2``,
-  FeaturesBodyText: styled.p``,
+  FeaturesHeading: styled.h2`
+    margin-bottom: 2.4rem;
+
+    font-weight: ${({ theme }) => theme.fontWeights.bold};
+    font-size: ${({ theme }) => theme.fontSizes.med2};
+    line-height: 3.6rem;
+    letter-spacing: 0.0857rem;
+    text-transform: uppercase;
+    color: ${({ theme }) => theme.colors.darkTitle};
+
+    ${({ theme }) => mediaQuery(theme.breakPoints.tabletLandscape)} {
+      margin-bottom: 3.2rem;
+
+      font-size: ${({ theme }) => theme.fontSizes.lg1};
+      line-height: 3.6rem;
+      letter-spacing: 0.1143rem;
+    }
+  `,
+  FeaturesBodyText: styled.p`
+    white-space: pre-wrap;
+
+    font-weight: ${({ theme }) => theme.fontWeights.medium};
+    font-size: ${({ theme }) => theme.fontSizes.regular};
+    line-height: 2.5rem;
+    color: ${({ theme }) => theme.colors.bodyTextDark};
+  `,
 }
 
 type FeaturesSectionProps = { data: string }
