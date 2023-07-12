5d989810b995c40593acfb07bb90cc150edf6d2e
thewanionly <pelloani@gmail.com>
2023-07-13 04:23:16 +0800

    test: add test for displaying you may also like section in pdp

--- src/app/(app)/[category]/[product]/page/ProductDetail.test.tsx
+++ src/app/(app)/[category]/[product]/page/ProductDetail.test.tsx
@@ -5,7 +5,12 @@ import { mockedBrandDetails } from '@/tests/__mocks__/data'
 import { mockedCategories } from '@/tests/__mocks__/data/category'
 import { mockedProduct, mockedSuggestedProducts } from '@/tests/__mocks__/data/product'
 
-import { FEATURES_SECTION_HEADING, GO_BACK, IN_THE_BOX_SECTION_HEADING } from '../utils/constants'
+import {
+  FEATURES_SECTION_HEADING,
+  GO_BACK,
+  IN_THE_BOX_SECTION_HEADING,
+  YOU_MAY_ALSO_LIKE,
+} from '../utils/constants'
 import { ProductDetail } from './ProductDetail'
 
 jest.mock('next/navigation', () => {
@@ -62,6 +67,13 @@ describe('Product Detail Page', () => {
 
     const gallerySection = screen.getByTestId('gallery-section')
     expect(gallerySection).toBeInTheDocument()
+  })
+
+  it('displays you may also like section', () => {
+    setup()
+
+    const youMayAlsoLikeSection = screen.getByRole('heading', { name: YOU_MAY_ALSO_LIKE })
+    expect(youMayAlsoLikeSection).toBeInTheDocument()
   })
 
   it('displays category card list section', () => {
