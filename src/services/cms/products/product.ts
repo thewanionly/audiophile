917527a8b7718e1ba1a9ea78b35f6de07c730cb6
thewanionly <pelloani@gmail.com>
2023-07-15 03:09:42 +0800

    refactor: remove unused getProducts function

--- src/services/cms/products/product.ts
+++ src/services/cms/products/product.ts
@@ -2,22 +2,6 @@ import { productClient } from '@/lib/cms/products'
 import { urlForImage } from '@/lib/cms/products/image'
 import { postProcessImage } from '@/utils/cms/image'
 
-// Get all products
-export const getProducts = async (): Promise<Product[]> => {
-  try {
-    const query = `*\[_type == "product"\] {
-        id,
-        slug,
-        name,
-        category,
-      }`
-
-    return await productClient.fetch(query)
-  } catch (error) {
-    throw error
-  }
-}
-
 // Get a single product
 export const getProduct = async (slug: string, fields: ProductField[]): Promise<Product> => {
   try {
