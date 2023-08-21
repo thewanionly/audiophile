c6d28fbbf80ba1024b0b38048c28b70f037a26a2
thewanionly <pelloani@gmail.com>
2023-08-21 22:32:19 +0800

    test: add missing tests in payment details section

--- src/app/(app)/checkout/page/CheckoutForm/PaymentDetails/PaymentDetails.test.tsx
+++ src/app/(app)/checkout/page/CheckoutForm/PaymentDetails/PaymentDetails.test.tsx
@@ -50,4 +50,106 @@ describe('PaymentDetails', () => {
       expect(codMessage).toBeInTheDocument()
     })
   })
+
+  describe('Interactions', () => {
+    it('can select only one of the payment method', async () => {
+      setup()
+
+      const eMoneyOption = screen.getByRole('radio', { name: PAYMENT_METHODS.eMoney.label })
+      const codOption = screen.getByRole('radio', { name: PAYMENT_METHODS.cod.label })
+
+      // Check eMoney option
+      await userEvent.click(eMoneyOption)
+      expect(eMoneyOption).toBeChecked()
+      expect(codOption).not.toBeChecked()
+
+      // Check cod option
+      await userEvent.click(codOption)
+      expect(eMoneyOption).not.toBeChecked()
+      expect(codOption).toBeChecked()
+    })
+
+    it.each`
+      field
+      ${'e-Money Number'}
+      ${'e-Money PIN'}
+    `('displays inputted value in $field field', async ({ field }) => {
+      const value = '12'
+      setup()
+
+      // Check eMoney option
+      const eMoneyOption = screen.getByRole('radio', { name: PAYMENT_METHODS.eMoney.label })
+      await userEvent.click(eMoneyOption)
+      expect(eMoneyOption).toBeChecked()
+
+      const fieldElement = screen.getByRole('textbox', { name: field })
+      await userEvent.type(fieldElement, value)
+
+      expect(fieldElement).toHaveValue(value)
+    })
+
+    it.each`
+      field
+      ${'e-Money Number'}
+      ${'e-Money PIN'}
+    `('can only input numeric values in $field field', async ({ field }) => {
+      setup()
+
+      // Check eMoney option
+      const eMoneyOption = screen.getByRole('radio', { name: PAYMENT_METHODS.eMoney.label })
+      await userEvent.click(eMoneyOption)
+      expect(eMoneyOption).toBeChecked()
+
+      const fieldElement = screen.getByRole('textbox', { name: field })
+
+      // Simulate user entering non-numeric values
+      userEvent.clear(fieldElement)
+      await userEvent.type(fieldElement, 'abc')
+      expect(fieldElement).toHaveValue('')
+
+      // Simulate user entering a numeric value
+      userEvent.clear(fieldElement)
+      await userEvent.type(fieldElement, '12')
+      expect(fieldElement).toHaveValue('12')
+    })
+
+    it.each`
+      field
+      ${'e-Money Number'}
+      ${'e-Money PIN'}
+    `(
+      `displays a required error message in $field field when it was touched and then blurred leaving it with an empty value`,
+      async ({ field }) => {
+        setup()
+
+        const fieldElement = screen.getByRole('textbox', { name: field })
+        await userEvent.click(fieldElement)
+        await userEvent.click(document.body)
+
+        expect(screen.getByText('Required')).toBeInTheDocument()
+      }
+    )
+
+    it.each`
+      field
+      ${'e-Money Number'}
+      ${'e-Money PIN'}
+    `(
+      `hides the required error message in $field field after non-empty value is inputted`,
+      async ({ field }) => {
+        setup()
+
+        const fieldElement = screen.getByRole('textbox', { name: field })
+
+        // Show required error message
+        await userEvent.click(fieldElement)
+        await userEvent.click(document.body)
+        expect(screen.getByText('Required')).toBeInTheDocument()
+
+        // Input numberic value to remove required error message
+        await userEvent.type(fieldElement, '12')
+        expect(screen.queryByText('Required')).not.toBeInTheDocument()
+      }
+    )
+  })
 })
